559431abf5adb3de7762700019463f9c1f0d1bc4
riskypaditya <donloadfilem@gmail.com>
2021-12-20 13:07:37 +0700

    add other table

---
+++ app/Models/JenisKonstruksiJalan.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JenisKonstruksiJalan extends Model
+{
+    use HasFactory;
+}
