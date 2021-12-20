85a1b1cf5a1198dada070e95ed2525b5d0b2bab6
Hendra Afrizal <hendraafrizal30@gmail.com>
2021-12-20 11:05:18 +0700

    menambah migrate table

---
+++ database/migrations/2021_12_20_040027_create_kecamatans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKecamatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kecamatans', function (Blueprint $table) {
+            $table->id('id');
+            $table->foreignId('kabupaten_id');
+            $table->string('nama');
+            $table->timestamps();
+
+            $table->foreign('kabupaten_id')->references('id')->on('kabupatens');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kecamatans');
+    }
+}
