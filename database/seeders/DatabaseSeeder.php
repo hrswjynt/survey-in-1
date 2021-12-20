ec5bda9de47eda49df2b2c4961a17e22581fb66a
Hendra Afrizal <hendraafrizal30@gmail.com>
2021-12-20 16:46:55 +0700

    add database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,19 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use App\Models\Fasos;
 use App\Models\User;
+use App\Models\Fasos;
+use App\Models\Provinsi;
+use App\Models\Kabupaten;
+use App\Models\Kecamatan;
+use App\Models\DataSurvey;
+use App\Models\JenisFasos;
+use App\Models\LampiranFoto;
+use App\Models\JenisLampiran;
 use App\Models\RiwayatSurvey;
+use Illuminate\Database\Seeder;
+use App\Models\JenisKonstruksiJalan;
+use App\Models\JenisKonstruksiSaluran;
 
 
 class DatabaseSeeder extends Seeder
@@ -18,8 +27,17 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        // Fasos::factory(10)->create();
         User::factory(20)->create();
+        JenisLampiran::factory(10)->create();
+        Provinsi::factory(10)->create();
+        Kabupaten::factory(10)->create();
+        Kecamatan::factory(10)->create();
+        JenisFasos::factory(10)->create();
+        JenisKonstruksiJalan::factory(10)->create();
+        JenisKonstruksiSaluran::factory(10)->create();
+        DataSurvey::factory(10)->create();
+        LampiranFoto::factory(10)->create();
+        Fasos::factory(10)->create();
         RiwayatSurvey::factory(20)->create();
     }
 }
