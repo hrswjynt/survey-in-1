a420bff61d77bca871adeeb211e0d7ae0b616f24
riskypaditya <donloadfilem@gmail.com>
2021-12-21 11:36:25 +0700

    s

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 
-class SurveyorController extends Controller
+class AdminController extends Controller
 {
     public function profile($id)
     {
