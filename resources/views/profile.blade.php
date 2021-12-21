83598b47af08e2b38ea498a7522215e90d7d76cf
Hendra Afrizal <hendraafrizal30@gmail.com>
2021-12-21 11:46:08 +0700

    .

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,21 +9,24 @@
     <link rel="stylesheet" href="/fontawesome5/css/all.css">
     <link rel="stylesheet" href="/css/profil-utama.css">
 </head>
+
 <body>
-    
-<div class="container">
+
+    <div class="container">
         <!-- sidebar -->
         <div class="sidebar" id="mySidebar">
-           <div class="side"> 
-            <div class="span" id="mySpan">
-                <input type="checkbox" />
-                <!-- <span></span>
+            <div class="side">
+                <div class="span" id="mySpan">
+                    <input type="checkbox" />
+                    <!-- <span></span>
                 <span></span>
                 <span></span> -->
-            </div>
-            <div class="logo">
-                <div class="img"><h2>Survei</h2></div>
-                <p>Aplikasi Survei Gang dan Perumahan  di Kota Pontianak</p>
+                </div>
+                <div class="logo">
+                    <div class="img">
+                        <h2>Survei</h2>
+                    </div>
+                    <p>Aplikasi Survei Gang dan Perumahan di Kota Pontianak</p>
                 </div>
                 <ul class="menu">
                     <li><a href=""><span class="icon b"></span>Beranda</a></li>
@@ -32,13 +36,13 @@
                     <li><a href=""><span class="icon e"></span>Pengaturan</a></li>
                     <li><a href="/"><span class="icon f"></span>Keluar</a></li>
                 </ul>
-           </div>     
+            </div>
         </div>
 
         <!-- sidebar end -->
 
 
-         <!-- Main Content -->
+        <!-- Main Content -->
         <div class="main-content">
             {{-- @dd($profile) --}}
             <!-- Header -->
@@ -47,7 +51,7 @@
                     <h1>Profile</h1>
                 </div>
                 <div class="subhead-b">
-                    <p>Profile Admin</p>
+                    <p>{{ $nama_lengkap }}</p>
                     <div class="profil-img"></div>
                     <div class="dropdown">
                         <button class="dropbtn"><i class="fas fa-angle-down"></i></button>
@@ -60,7 +64,7 @@
             </div>
             <hr solid>
 
-           <!-- Content -->
+            <!-- Content -->
             <div class="content">
                 <div class="admin-hl">
                     <h1>Profile Admin</h1>
@@ -68,42 +72,42 @@
                     <div class="admin-img">
                         <div class="admin-foto">
                         </div>
-                        <h2>Azizi Iqbalika</h2>
-                    <p class="status">Admin</p>
+                        <h2>{{ $nama_lengkap }}</h2>
+                        <p class="status">{{ $role }}</p>
                     </div>
-                    
+
                 </div>
                 <!-- ===================== -->
                 <div class="biodata">
-                        <table class="bio">
-                            <tr>
-                                <td class="left-bio">Nama Lengkap</td>
-                                <td class="right-bio">: {{ $nama_lengkap }}</td>
-                            </tr>
-                            <tr>
-                                <td class="left-bio">NIP</td>
-                                <td class="right-bio">: 200105172020032003</td>
-                            </tr>
-                            <tr>
-                                <td class="left-bio">Email</td>
-                                <td class="right-bio">: {{ $email }}</td>
-                            </tr>
-                            <tr>
-                                <td class="left-bio">Jenis Kelamin</td>
-                                <td class="right-bio">: {{ $gender }}</td>
-                            </tr>
-                            <tr>
-                                <td class="left-bio">No. Handphone</td>
-                                <td class="right-bio">: {{ $nomor_telepon }}</td>
-                            </tr>
-                            <tr style="border: none;">
-                                <td class="left-bio">Alamat</td>
-                                <td class="right-bio">: Jeruju</td>
-                            </tr>         
-                        </table>
-                        <div class="button">
+                    <table class="bio">
+                        <tr>
+                            <td class="left-bio">Nama Lengkap</td>
+                            <td class="right-bio">: {{ $nama_lengkap }}</td>
+                        </tr>
+                        <tr>
+                            <td class="left-bio">NIP</td>
+                            <td class="right-bio">: 200105172020032003</td>
+                        </tr>
+                        <tr>
+                            <td class="left-bio">Email</td>
+                            <td class="right-bio">: {{ $email }}</td>
+                        </tr>
+                        <tr>
+                            <td class="left-bio">Jenis Kelamin</td>
+                            <td class="right-bio">: {{ $gender }}</td>
+                        </tr>
+                        <tr>
+                            <td class="left-bio">No. Handphone</td>
+                            <td class="right-bio">: {{ $nomor_telepon }}</td>
+                        </tr>
+                        <tr style="border: none;">
+                            <td class="left-bio">Alamat</td>
+                            <td class="right-bio">: Jeruju</td>
+                        </tr>
+                    </table>
+                    <div class="button">
                         <button class="btn-bio">Edit Profil</button>
-                        </div>
+                    </div>
                 </div>
 
             </div>
@@ -117,15 +121,16 @@
 
         <div class="modal-container" id="modal_container">
             <div class="modal">
-            
+
                 <p>Anda yakin ingin keluar<br>dari aplikasi ini ?</p>
                 <button id="close">Keluar</button>
                 <button id="cancel">Batal</button>
             </div>
         </div>
     </div>
-                    <!-- Main Content End -->
+    <!-- Main Content End -->
     <script src="/js/script.js"></script>
     <script src="/js/modal.js"></script>
 </body>
+
 </html>
