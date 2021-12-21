1682dc39d313a30b2d46e150d36354a88ab3f0b2
riskypaditya <donloadfilem@gmail.com>
2021-12-21 11:19:16 +0700

    dasd

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,15 +15,21 @@
         <!-- container-form-login -->
         <div class="login-form">
             <h1>Mari kita mulai</h1>
-            <form action="" method="post" class="login">
-                <p>Username</p>
-                <input type="text" name="username" id="id_username">
-                <p>Password</p>
-                <input type="password" name="password" id="id_password">
-                <i class="far fa-eye" id="togglePassword"></i>
-                <a href="">Lupa kata sandi ?</a>
-            </form>
-            <button type="submit" class="btn btn-masuk">Masuk</button>
+            <form>
+                <div class="mb-3">
+                  <label for="email" class="form-label">Email address</label>
+                  <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                  <label for="password" class="form-label">Password</label>
+                  <input type="password" class="form-control" id="password">
+                </div>
+                {{-- <div class="mb-3 form-check">
+                  <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                  <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div> --}}
+                <button type="submit" class="btn btn-primary">Login</button>
+              </form>
         </div>
         <!-- end container-form-login -->
 
