42943a3b80049fe8ccf36d582d4a2ac3c7bc7ef0
Hendra Afrizal <hendraafrizal30@gmail.com>
2021-12-29 22:01:21 +0700

    hehe js

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -93,17 +93,6 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/js/bootstrap.min.js"
         integrity="sha384-5h4UG+6GOuV9qXh6HqOLwZMY4mnLPraeTrjT5v07o347pj6IkfuoASuGBhfDsp3d" crossorigin="anonymous">
     </script>
-    <script src="/js/jquery-3.6.0.js"></script>
-    <!-- custom js -->
-    <script>
-        var menu_btn = document.querySelector("#menu-btn")
-        var sidebar = document.querySelector("#sidebar")
-        var container = document.querySelector(".my-container")
-        menu_btn.addEventListener("click", () => {
-            sidebar.classList.toggle("active-nav")
-            container.classList.toggle("active-cont")
-        })
-    </script>
 </body>
 
 </html>
